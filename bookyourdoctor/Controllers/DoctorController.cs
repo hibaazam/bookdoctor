b54ca1baf216a619e5fe18549aef3969f14809b8
Hiba Azam <hibaazam7@gmail.com>
2018-10-20 00:42:08 +0500

    extracting project

---
+++ bookyourdoctor/Controllers/DoctorController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bookyourdoctor.Controllers
+{
+    public class DoctorController : Controller
+    {
+        // GET: Doctor
+        public ActionResult Index()
+        {
+            RedirectToAction("DoctorDescription", "Doctor");
+            return View();
+        }
+        public ActionResult Registrationform()
+        {
+            return View();
+        }
+        public ActionResult Loginform()
+        {
+            return View();
+        }
+
+        // GET: Doctor/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+        public ActionResult DoctorDescription()
+        {
+           
+            return View();
+        }
+        public ActionResult DoctorClinicSchedule()
+        {
+            return View();
+        }
+        public ActionResult DoctorHospitalSchedule()
+        {
+            return View();
+        }
+        public ActionResult DoctorHospitalRequests()
+        {
+            return View();
+        }
+        public ActionResult DoctorClinicRequests()
+        {
+            return View();
+        }
+
+        // GET: Doctor/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Doctor/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Doctor/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Doctor/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Doctor/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Doctor/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
